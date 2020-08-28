eb372af42171734a04c7b7554fda65ecac7382c7
Brian Green <b.agreen92@gmail.com>
2020-08-28 09:04:49 -0700

    moved the header to above the video on the homepage, and changed the colour of the cart item number.

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -10,16 +10,17 @@ import homePageVideo from '../../assets/Nunzziii-movie2.mov';
 
 const HomePage = () => (
     <div className='homepage'>
+        <h1 className='homepage-heading'>
+            Nunzziii
+        </h1>
+        <div className='underline'></div>
+        
         <div className='homepage-video'>
             <video className='homepage-video__content' autoPlay muted loop>
                 <source src={video} type='video/mp4' />
                 The video is not supported on your browser.
             </video>
         </div>
-        <h1 className='homepage-heading'>
-            Nunzziii
-        </h1>
-        <div className='underline'></div>
         <h3 className='tagline'>
             If you're not excited, get excited!!
         </h3>
